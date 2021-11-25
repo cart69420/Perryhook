19b871e419631d6b1d0e894cf2c131d7734c7db2
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-11-24 22:10:43 -0800

    Length Check.
    Time to see if it works since I'm not even using a IDE for any of this kek.

--- Perryhook.py
+++ Perryhook.py
@@ -54,6 +54,12 @@ def initialize():
     delay = input("Enter a delay [int/float] > ")
     amount = input("Enter an amount [int/inf] > ")
     hookDeleter = input("Delete webhook after spam? [Y/N] > ")
+    if len(message) > 2000:
+        print "Error! Only 2000 characters allowed in the message!"
+        _exit()
+    elif len(username) > 80:
+        print "Error! Only 80 characters allowed in the username!"
+        _exit()
     try:
         delay = float(delay)
     except ValueError:
