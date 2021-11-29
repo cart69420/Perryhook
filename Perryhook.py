7dbe1575ad97c7ca2d47febbc381f357bf9271ed
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-11-28 21:51:16 -0800

    Update Perryhook.py
    Tiny bit of documentation.

--- Perryhook.py
+++ Perryhook.py
@@ -20,11 +20,13 @@ def main(webhook, username, avatar, delay, amount, message, deleter):
                                  json={"content": str(message), "username": str(username), "avatar_url": str(avatar)})
             if data.status_code == 204:
                 print(f"{colorama.Back.MAGENTA} {colorama.Fore.WHITE}[+] Sent{colorama.Back.RESET}")
+# Simple but effective rate limiting system so u can spam forever until they or u delete the webhook.
             elif data.status_code == 429:
                 timeout = int(str(data.json()['retry_after']))
                 print(
                     f"{colorama.Back.RED} {colorama.Fore.WHITE}[-] Failed Ratelimited Waiting {timeout / 1000}s{colorama.Back.RESET}")
                 time.sleep(timeout / 1000)
+# Checks during sending stuff if the webhook was deleted and if it was it alerts u so u know u aren't wasting ur time.
             if "\"message\": \"Unknown Webhook\"" in requests.get(webhook).text:
                 print(f"{colorama.Back.RED} {colorama.Fore.WHITE}[-] Skids deleted the webhook.{colorama.Back.RESET}")
                 exit()
@@ -50,12 +52,14 @@ def initialize():
      """)
     webhook = input("Enter ur webhook > ")
     username = input("Enter a webhook name > ")
+# Checks the length of the username input and if its over 80 it alerts u because in normal Discord without nitro that wouldn't work and usernames thru webhooks cant have nitro so it limits to 80 characters and simply closes/exits the program.
     if len(username) > 80:
         print(
             f"{colorama.Back.RED}{colorama.Fore.WHITE}Error! Only 80 characters allowed in the username!{colorama.Back.RESET}")
         exit()
     avatar = input("Enter a avatar url > ")
     message = input("Enter a message > ")
+# Checks the length of the message input and if its over 2000 it alerts u because in normal Discord without nitro that wouldn't work and sending messages thru webhooks cant have nitro so it limits to 2000 characters and simply closes/exits the program.
     if len(message) > 2000:
         print(
             f"{colorama.Back.RED}{colorama.Fore.WHITE}Error! Only 2000 characters allowed in the message!{colorama.Back.RESET}")
