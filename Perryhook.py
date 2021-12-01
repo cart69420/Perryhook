c3da3ee62ec1386dad458b655eae58434250a168
notperry1234567890 <65968863+notperry1234567890@users.noreply.github.com>
2021-12-01 04:11:14 -0800

    Asynchronous
    Cleanup & Asynchronous now (supposedly makes it faster personally I saw no difference but idk lol).

--- Perryhook.py
+++ Perryhook.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import time
 
@@ -5,14 +6,13 @@ import colorama
 import requests
 
 
-def check_hook(webhook):
-    info = requests.get(webhook).text
-    if "\"message\": \"Unknown Webhook\"" in info:
+async def check_hook(webhook):
+    if "\"message\": \"Unknown Webhook\"" in requests.get(webhook).text:
         return False
     return True
 
 
-def main(webhook, username, avatar, delay, amount, message, deleter):
+async def main(webhook, username, avatar, delay, amount, message, deleter):
     counter = 0
     while True if amount == "inf" else counter < int(amount):
         try:
@@ -20,13 +20,13 @@ def main(webhook, username, avatar, delay, amount, message, deleter):
                                  json={"content": str(message), "username": str(username), "avatar_url": str(avatar)})
             if data.status_code == 204:
                 print(f"{colorama.Back.MAGENTA} {colorama.Fore.WHITE}[+] Sent{colorama.Back.RESET}")
-# Simple but effective rate limiting system so u can spam forever until they or u delete the webhook.
+            # Simple but effective rate limiting system so u can spam forever until they or u delete the webhook.
             elif data.status_code == 429:
                 timeout = int(str(data.json()['retry_after']))
                 print(
                     f"{colorama.Back.RED} {colorama.Fore.WHITE}[-] Failed Ratelimited Waiting {timeout / 1000}s{colorama.Back.RESET}")
                 time.sleep(timeout / 1000)
-# Checks during sending stuff if the webhook was deleted and if it was it alerts u so u know u aren't wasting ur time.
+            # Checks during sending stuff if the webhook was deleted and if it was it alerts u so u know u aren't wasting ur time.
             if "\"message\": \"Unknown Webhook\"" in requests.get(webhook).text:
                 print(f"{colorama.Back.RED} {colorama.Fore.WHITE}[-] Skids deleted the webhook.{colorama.Back.RESET}")
                 exit()
@@ -40,7 +40,7 @@ def main(webhook, username, avatar, delay, amount, message, deleter):
     print(f'{colorama.Fore.GREEN}Done...')
 
 
-def initialize():
+async def initialize():
     print(f"""{colorama.Fore.MAGENTA} 
 ██████╗░███████╗██████╗░██████╗░██╗░░░██╗██╗░░██╗░█████╗░░█████╗░██╗░░██╗
 ██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗░██╔╝██║░░██║██╔══██╗██╔══██╗██║░██╔╝
@@ -48,18 +48,18 @@ def initialize():
 ██╔═══╝░██╔══╝░░██╔══██╗██╔══██╗░░╚██╔╝░░██╔══██║██║░░██║██║░░██║██╔═██╗░
 ██║░░░░░███████╗██║░░██║██║░░██║░░░██║░░░██║░░██║╚█████╔╝╚█████╔╝██║░╚██╗
 ╚═╝░░░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚═╝
-  I love Perry & Perryhook huge brain autistic 14 yr.
+  I love Perryhook.
      """)
     webhook = input("Enter ur webhook > ")
     username = input("Enter a webhook name > ")
-# Checks the length of the username input and if its over 80 it alerts u because in normal Discord without nitro that wouldn't work and usernames thru webhooks cant have nitro so it limits to 80 characters and simply closes/exits the program.
+    # Checks the length of the username input and if its over 80 it alerts u because in normal Discord without nitro that wouldn't work and usernames thru webhooks cant have nitro so it limits to 80 characters and simply closes/exits the program.
     if len(username) > 80:
         print(
             f"{colorama.Back.RED}{colorama.Fore.WHITE}Error! Only 80 characters allowed in the username!{colorama.Back.RESET}")
         exit()
     avatar = input("Enter a avatar url > ")
     message = input("Enter a message > ")
-# Checks the length of the message input and if its over 2000 it alerts u because in normal Discord without nitro that wouldn't work and sending messages thru webhooks cant have nitro so it limits to 2000 characters and simply closes/exits the program.
+    # Checks the length of the message input and if its over 2000 it alerts u because in normal Discord without nitro that wouldn't work and sending messages thru webhooks cant have nitro so it limits to 2000 characters and simply closes/exits the program.
     if len(message) > 2000:
         print(
             f"{colorama.Back.RED}{colorama.Fore.WHITE}Error! Only 2000 characters allowed in the message!{colorama.Back.RESET}")
@@ -71,11 +71,11 @@ def initialize():
         delay = float(delay)
     except ValueError:
         exit()
-    if not check_hook(webhook) or (not amount.isdigit() and amount != "inf") or (
+    if not await check_hook(webhook) or (not amount.isdigit() and amount != "inf") or (
             deleter.lower() != "y" and deleter.lower() != "n"):
         exit()
     else:
-        main(webhook, username, avatar, delay, amount, message, deleter)
+        await main(webhook, username, avatar, delay, amount, message, deleter)
         exit()
 
 
@@ -83,4 +83,4 @@ if __name__ == '__main__':
     os.system('cls')
     os.system('title Perryhook on top.')
     colorama.init()
-    initialize()
+    asyncio.run(initialize())
